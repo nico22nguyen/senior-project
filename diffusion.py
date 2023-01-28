2c8d7de2b568993efaf0afc4f3870e3b763b86d0
Nic <nico.nguyen22@gmail.com>
2023-01-28 14:20:45 -0600

    comments

--- diffusion.py
+++ diffusion.py
@@ -7,7 +7,9 @@ def noise_image(image, timestep):
   ones = tf.ones_like(image, dtype=tf.float32)
   random_normal = tf.random.normal(shape=image.shape, mean=0, stddev=ones, dtype=tf.float32)
 
+  # generate mu and sigma that define the noise distribution
   mu = (alphas_cumulative ** 0.5)[timestep] * image
   sigma = (1 - alphas_cumulative[timestep]) * random_normal
 
+  # return the noised image
   return tf.random.normal(shape=image.shape, mean=mu, stddev=sigma, dtype=tf.float32)
