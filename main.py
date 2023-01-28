4e47c17c6d1adbbf35401573f88c0d9facc91d9d
Nic <nico.nguyen22@gmail.com>
2023-01-28 15:24:35 -0600

    train, sample, display nine samples

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import keras.datasets.mnist as mnist
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from diffusion import UNet
+import numpy as np
+from UNet import UNet
 
 (x_train, _), (x_test, y_test) = mnist.load_data()
 x_train = x_train / 255 # tf.expand_dims(x_train, axis=-1) / 255
 
 model = UNet()
-out = model.call(tf.reshape(x_train[:1], [1, 28, 28, 1]))
 
-plt.figure()
-plt.imshow(out[0, :, :, 0], cmap='gray')
+model.train(x_train, epochs=1, batch_size=64)
+sample = model.sample(tf.random.normal(shape=(9, 28, 28)))
+
+fig, axes = plt.subplots(3, 3)
+for img, axis in zip(sample, np.ndarray.flatten(axes)):
+  axis.imshow(img, cmap='gray')
+  
 plt.show()
