a8492f947775f18e3b6abf3a1b4f62ed964c4c62
Nic <nico.nguyen22@gmail.com>
2023-01-28 15:24:13 -0600

    spacing

--- diffusion.py
+++ diffusion.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 timesteps = 100
 betas = tf.linspace(0., 1, timesteps)
 alphas_cumulative = tf.math.cumprod(1 - betas, axis=0) ** 0.5
+
 def noise_image(image, timestep):
   ones = tf.ones_like(image, dtype=tf.float32)
   random_normal = tf.random.normal(shape=image.shape, mean=0, stddev=ones, dtype=tf.float32)
