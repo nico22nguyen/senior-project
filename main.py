a0ae118851b7cfed4434c311ef80db90a2862362
Nic <nico.nguyen22@gmail.com>
2023-02-28 17:32:04 -0600

    added support for all datasets in main.py

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import keras.datasets.mnist as mnist
 import matplotlib.pyplot as plt
 from UNet import UNet
-import tensorflow as tf
 import numpy as np
 
-shoes = np.load('data/shoes.npy')
-cats_dogs = np.load('data/cats_dogs.npy')
-faces = np.load('data/faces.npy')
+network_code = input('Which network would you like to train?\n\t1. MNIST\n\t2. Shoes\n\t3. Cats and Dogs\n\t4. Faces\n')
 
-(x_train, _), (x_test, y_test) = mnist.load_data()
-x_train = x_train / 255
+# load data
+if network_code == '1':
+  (x_train, _), (x_test, _) = mnist.load_data()
+  data = np.concatenate((x_train, x_test))
+elif network_code == '2':
+  data = np.load('data/shoes.npy')
+elif network_code == '3':
+  data = np.load('data/cats_dogs.npy')
+elif network_code == '4':
+  data = np.load('data/faces.npy')
 
-model = UNet()
-model.train(x_train, epochs=3, batch_size=16, show_samples=True, show_losses=True)
+# normalize to [0, 1]
+data = data / 255
 
-# trained
+# add channel dimension if necessary
+if len(data.shape) != 4:
+  data = np.expand_dims(data, axis=-1)
+
+model = UNet(image_shape=data[0].shape)
+model.train(data, epochs=5, batch_size=32, show_samples=True, show_losses=True)
+
 """
 denoised = tf.random.normal(shape=x_train[0].shape)
 fig, axs = plt.subplots(10, 10)
