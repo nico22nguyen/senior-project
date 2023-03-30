e22f67de658f1f20a2f9a6a5293b2cdef7b6816b
Nic <nico.nguyen22@gmail.com>
2023-03-29 21:15:30 -0500

    added noise seed output to noiser function, so that identical copies can be generated

--- noiser.py
+++ noiser.py
@@ -1,15 +1,18 @@
 import tensorflow as tf
 
 TIMESTEPS = 100
-BETAS = tf.linspace(0.0001, 0.02, TIMESTEPS)
+BETAS = tf.linspace(1e-4, 6e-2, TIMESTEPS)
 alphas_cumulative = tf.math.cumprod(1 - BETAS, axis=0)
 # alphas_cumulative = tf.concat((tf.constant([1.]), alphas_cumulative[:-1]), axis=0)
 sqrt_alphas = alphas_cumulative ** 0.5
 sqrt_one_minus_alphas = (1 - alphas_cumulative) ** 0.5
 
-def noise_images(images, _timesteps):
+def noise_images(images, _timesteps, starting_noise=None):
   _images = tf.cast(images, tf.float32)
-  random_normal = tf.random.normal(shape=_images.shape)
+  if starting_noise is None:
+    random_normal = tf.random.normal(shape=_images.shape)
+  else:
+    random_normal = starting_noise
 
   # get the alphas and one_minus_alphas for each timestep
   selected_sqrt_alphas = tf.gather(sqrt_alphas, _timesteps)
@@ -21,5 +24,5 @@ def noise_images(images, _timesteps):
   # modify noise based on timestep
   modified_noise = tf.einsum('i,ijkl->ijkl', selected_sqrt_one_minus_alphas, random_normal)
 
-  # return sum
-  return modified_images + modified_noise
+  # return sum and noise
+  return (modified_images + modified_noise, random_normal)
