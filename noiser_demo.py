a465e56bcb96afb81f2ea9c2d3e1ae7453527bb1
Nic <nico.nguyen22@gmail.com>
2023-03-29 21:11:47 -0500

    cleaned up noiser_demo

--- noiser_demo.py
+++ noiser_demo.py
@@ -4,33 +4,43 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from noiser import noise_images
 
-NUM_SAMPLES = 3
+NUM_SLIDES = 1
 MAX_TIME = 100
-NUM_PROGRESSIONS = 25
+NUM_PROGRESSIONS = 100
+
+square_size = math.sqrt(NUM_PROGRESSIONS)
+NUM_ROWS = math.floor(square_size)
+NUM_COLS = math.ceil(square_size)
 
 STEP_SIZE = math.ceil(MAX_TIME / NUM_PROGRESSIONS)
 
-(x_train, _), (x_test, y_test) = mnist.load_data()
-x_train = 2 * (x_train[:NUM_SAMPLES] / 255) - 1
+(data, _), (x_test, y_test) = mnist.load_data()
+# only select the amount of samples as we need slides
+data = data[:NUM_SLIDES]
+# normalize to [-1, 1]
+data = 2 * (data / 255) - 1
 
-noised_image_progressions = []
-for (i, image) in enumerate(x_train):
-  repeated_image = tf.repeat(tf.expand_dims(image, axis=0), repeats=NUM_PROGRESSIONS, axis=0)
+slides = []
+for image in data:
+  # add batch dimension
+  batched_image = tf.expand_dims(image, axis=0)
+
+  # repeat image as many times as we need progression steps
+  repeated_image = tf.repeat(batched_image, repeats=NUM_PROGRESSIONS, axis=0)
 
   # add channel dimension if necessary
   if len(repeated_image.shape) != 4:
     repeated_image = tf.expand_dims(repeated_image, axis=-1)
 
   # noise the same image at different timesteps
-  noise_progression = noise_images(repeated_image, range(0, MAX_TIME, STEP_SIZE))
-  noised_image_progressions.append(noise_progression)
+  noise_progression = noise_images(repeated_image, range(0, MAX_TIME, STEP_SIZE))[0]
+  slides.append(noise_progression)
 
-for sample_index, progression in enumerate(noised_image_progressions):
-  square_size = math.sqrt(NUM_PROGRESSIONS)
-  num_rows = math.floor(square_size)
-  num_cols = math.ceil(square_size)
-  fig, axs = plt.subplots(num_rows, num_cols)
+# draw slides
+for sample_index, progression in enumerate(slides):
+  fig, axs = plt.subplots(NUM_ROWS, NUM_COLS)
   fig.suptitle(f'Sample {sample_index + 1}')
   for timestep, image in enumerate(progression):
-    axs[timestep // num_cols][timestep % num_cols].imshow(image, cmap='gray')
+    axs[timestep // NUM_COLS][timestep % NUM_COLS].imshow(image, cmap='gray')
+
 plt.show()
