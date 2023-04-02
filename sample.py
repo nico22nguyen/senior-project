7895bda1a757f7df362e2826e9b731b0b742cc10
Nic <nico.nguyen22@gmail.com>
2023-04-02 18:58:15 -0500

    uisf

--- sample.py
+++ sample.py
@@ -2,6 +2,7 @@ from medium_code import Unet #UNet import UNet
 import tensorflow as tf
 from plotter import show_sample_process
 from matplotlib import pyplot as plt
+import noiser
 
 print('instantiating model...')
 model = Unet(channels=1)# UNet(image_shape=(28, 28, 1))
@@ -23,12 +24,12 @@ print('done!')
 """
 
 print('generating samples...')
-samples = model.sample2()
+samples = model.sample()
 
 print('showing results...')
 plt.ion()
 for (i, sample) in enumerate(samples):
-  plt.suptitle(f'Timestep {100 - i}')
+  plt.suptitle(f'Timestep {noiser.TIMESTEPS - i}')
   plt.imshow(tf.squeeze(sample), cmap='gray')
   plt.show()
   plt.pause(0.01)
