421c2331f49783168baa0e9cb843aa163f2cee93
luc nguyen <nico.nguyen22@gmail.com>
2023-04-21 12:20:49 -0500

    finished merge

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import noiser
 import plotter
+import time
+
+network_code = '4'# input('Which network would you like to train?\n\t1. MNIST\n\t2. Shoes\n\t3. Cats and Dogs\n\t4. Faces\n')
 
 def preprocess(x: tf.Tensor, target_shape, limit_num_samples_to=None) -> tf.Tensor:
   if len(target_shape) != 2:
@@ -36,34 +39,39 @@ def ask_to_save():
 network_code = input('Which network would you like to train?\n\t1. MNIST\n\t2. Shoes\n\t3. Cats and Dogs\n\t4. Faces\n')
 target_shape = (32, 32)
 channels = 1
+save_file = ''
 
 # load data
 if network_code == '1':
   (x_train, _), (x_test, _) = mnist.load_data()
   data = np.concatenate((x_train, x_test))
   target_shape = (32, 32)
+  save_file = 'models/mnist.pkl'
 elif network_code == '2':
   data = np.load('data/shoes.npy')
-  target_shape = (36, 48)
+  target_shape = (48, 64)
   channels = 3
+  save_file = 'models/shoes.pkl'
 elif network_code == '3':
   data = np.load('data/cats_dogs.npy')
   target_shape = (64, 64)
+  save_file = 'models/cats_dogs.pkl'
 elif network_code == '4':
   data = np.load('data/faces.npy')
-  target_shape = (72, 72)
+  target_shape = (80, 80)
   channels = 3
+  save_file = 'models/faes.pkl'
 
 # normalize to [-1, 1], resize
 data = preprocess(data, target_shape=target_shape)
-
-# add channel dimension if necessary
-if len(data.shape) != 4:
-  data = np.expand_dims(data, axis=-1)
-
 image_shape = (target_shape[0], target_shape[1], channels)
+
 model = UNet(channels=channels, image_shape=image_shape)
-model.train(data, show_samples=False, show_losses=False, epochs=10, batch_size=16)
+start_time = time.perf_counter()
+model.train(data, show_samples=False, show_losses=False, epochs=25, batch_size=10)
+end_time = time.perf_counter()
+print(f'model trained in {end_time - start_time} seconds')
+model.save_weights(save_file)
 
 plt.ion()
 while True:
